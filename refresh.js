04c556099101e162f53cd10754e52c287e25e92e
aglr <aymeric.gerlier@rentacar.fr>
2018-08-30 00:19:22 +0200

    update prerender refresh

--- refresh.js
+++ refresh.js
@@ -1,47 +1,16 @@
-/*
 window.addEventListener('load', function(ev) {
-    const xhr = new XMLHttpRequest();
-    const body = document.body;
+    const nextUrl = './index.html?' + new Date().getTime();
 
-    xhr.onreadystatechange = function() {
-        if(4 === xhr.readyState && 200 === xhr.status) {
-            const xmlDoc = xhr.responseXML;
-            const imgs = xmlDoc.getElementsByTagName('img');
-            const imgsSize = imgs.length;
-            const articles = xmlDoc.getElementsByTagName('article');
-            const articlesSize = articles.length;
+    window.setTimeout(function() {
+        const prerender = document.createElement('link');
+        
+        prerender.setAttribute('rel', 'prerender');
+        prerender.setAttribute('href', nextUrl);
 
-            let imagesLeft = imgsSize;
+        document.head.appendChild(prerender);
 
-            for (let i=0; i<imgsSize; i++) {
-                const img = imgs.item(i);
-                const cache = new Image();
-
-                cache.onload = function() {
-                    if (0 === --imagesLeft) {
-                        for (let j=0; j<articlesSize; j++) {
-                            const article = articles.item(j);
-                            body.appendChild(article);
-                        }
-
-                        console.log("refresh");
-            
-                        while (body.childNodes.length > articlesSize) {
-                            body.removeChild(body.firstChild);
-                        }
-                    }
-                }
-
-                console.log("src="+img.getAttribute("src"));
-
-                cache.src = img.getAttribute("src");
-            }
-        }
-    }
-
-    window.setInterval(function() {
-        xhr.open('GET', './index.html', true);
-        xhr.send();
-    }, 3000);
+        window.setTimeout(function() {
+            window.top.location.href = nextUrl;
+        }, 5000)
+    }, 25000);
 });
-*/
