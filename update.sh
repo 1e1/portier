3f4dbb242025dbc3a74bdecf8a9726a44a3ab6f9
aglr <aymeric.gerlier@rentacar.fr>
2018-08-31 14:35:48 +0200

    add index generator

--- update.sh
+++ update.sh
@@ -2,38 +2,46 @@
 
 # http://manpages.ubuntu.com/manpages/bionic/en/man8/start-stop-daemon.8.html
 
+INDEX=True
 DAEMON=False
 SLEEP=15
 BASE_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )
-DIR="$BASE_DIR/pages"
-INDEX="$BASE_DIR/index.html"
+INDEX_PATH="$BASE_DIR/index.html"
+DIR_PATH="$BASE_DIR/pages"
 FILES=()
 HTML=()
+LINKS=()
 
 
 
 for opt in $*
 do
   case $opt in
+  "-s"|"--no-index")
+    INDEX=False
+    ;;
   "-d"|"--daemon")
     DAEMON=True
     ;;
   --sleep=*)
     SLEEP=${opt#*=}
     ;;
+  --index=*)
+    INDEX_PATH=${opt#*=}
+    ;;
   --dir=*)
-    DIR=${opt#*=}
+    DIR_PATH=${opt#*=}
     ;;
   esac
 done
 
 
 
-if [ -f "$DIR" ]
+if [ -f "$DIR_PATH" ]
 then
-  FILES=($DIR)
+  FILES=($DIR_PATH)
 else
-  FILES="$DIR/*"
+  FILES="$DIR_PATH/*"
 fi
 
 
@@ -78,31 +86,51 @@ xx()
 
 
 echo 'links:'
-
-HTML+=('<html>')
-HTML+=('<body background="#EEE">')
-HTML+=('<h1>pages</h1>')
-HTML+=('<hr/>')
-HTML+=('<ul>')
 for f in $FILES
 do
-  echo "realpath --relative-to='$BASE_DIR' '$f'"
   LINK=$(realpath --relative-to="$BASE_DIR" "$f")
-  echo "-> $LINK"
-  HTML+=("<li><a href='$LINK'>$LINK</a></li>")
+  LINKS+=($LINK)
+  echo "- $LINK"
 done
-HTML+=('</ul>')
-HTML+=('<hr/>')
+echo
+
+HTML+=('<html>')
+HTML+=('<head>')
+HTML+=('<link rel="stylesheet" href="./assets/style.css"/>')
+for l in ${LINKS[*]}
+do
+  HTML+=("<link rel="preload" href='$l'/>")
+  HTML+=("<link rel="prerender" href='$l'/>")
+done
+HTML+=('</head>')
+HTML+=('<body>')
+HTML+=('<section class="index flex">')
+for l in ${LINKS[*]}
+do
+  HTML+=("<a href='$l'>$l</a>")
+done
+HTML+=('</section>')
+HTML+=('<section class="news"><ul><li>')
 HTML+=($(date "+%Y-%m-%d %X"))
+HTML+=('</li></ul></section>')
 HTML+=('</body>')
 HTML+=('</html>')
 
-echo ${HTML[*]} > $INDEX
-echo 
+
 
 if [ $DAEMON = True ]
 then
   xx
 else
   x
+fi
+
+if [ $INDEX = True ]
+then
+  echo ${HTML[*]} > $INDEX_PATH
+  echo "index: $INDEX_PATH"
+  echo 
+else
+  echo 'index: none'
+  echo
 fi
