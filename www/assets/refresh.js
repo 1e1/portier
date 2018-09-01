af6e5558b9e392182dd7bbf10108eb2e4e504bad
aglr <aymeric.gerlier@rentacar.fr>
2018-09-01 11:25:03 +0200

    repaint only if data changed

--- www/assets/refresh.js
+++ www/assets/refresh.js
@@ -1,45 +1,64 @@
-function refreshContent(domXml) {
+const xhr = new XMLHttpRequest();
+
+const refreshContent = (domXml) => {
     const article = domXml.querySelector('article');
     const body = document.querySelector('body');
     
     body.innerHTML = article.outerHTML;
 }
 
-function selfReload(url) {
-    const xhr = new XMLHttpRequest();
-
-    xhr.onreadystatechange = function() {
-        if (4 === this.readyState) {
-            const domXml = this.responseXML;
-            const imgs = domXml.getElementsByTagName('img');
-            const size = imgs.length;
-            let count = 0;
-
-            for (let i=0; i<size; ++i) {
-                const img = imgs.item(i);
-                const href = img.getAttribute('src');
-                const cache = new Image();
-                cache.onload = function() {
-                    if (++count === size) {
-                        refreshContent(domXml);
-                    }
-                }
-                cache.src = href;
-            }
-        }
-    }
-
+const selfReload = url => {
     xhr.open('GET', url.toString(), true);
     xhr.send();
 }
 
+String.prototype.hashCode = function() {
+    let hash = 0;
+    for (let i = 0; i < this.length; i++) {
+        const chr   = this.charCodeAt(i);
+        hash  = ((hash << 5) - hash) + chr;
+        hash |= 0; // Convert to 32bit integer
+    }
 
-window.addEventListener('load', function(ev) {
+    return hash;
+}
+
+window.addEventListener('load', ev => {
     const url = new URL(window.location.href);
+    let previousHashCode = 0;
 
-    url.search = new Date().getTime();
+    xhr.onreadystatechange = (rsc) => {
+        const target = rsc.target;
 
-    window.setInterval(function() {
+        if (4 === target.readyState) {
+            const hashCode = target.responseText.hashCode();
+
+            if (previousHashCode !== hashCode) {
+                previousHashCode = hashCode;
+
+                const domXml = target.responseXML;
+                const imgs = domXml.getElementsByTagName('img');
+                const size = imgs.length;
+                let count = 0;
+        
+                for (let i=0; i<size; ++i) {
+                    const img = imgs.item(i);
+                    const href = img.getAttribute('src');
+                    const cache = new Image();
+        
+                    cache.onload = function() {
+                        if (++count === size) {
+                            refreshContent(domXml);
+                        }
+                    }
+                    cache.src = href;
+                }
+            }
+        }
+    }
+
+    window.setInterval(() => {
+        //url.search = new Date().getTime();
         selfReload(url);
-    }, 10000);
+    }, 2500);
 });
